e80e40e2af554286b98ccb155aae77dab7df141d
Anmol GR <anmol.grao@gmail.com>
2021-05-16 23:21:26 +0800

    update files

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 $('html,body').animate({scrollTop: 80000});
+
+// $("a[href='#bottom']").click(function() {
+//   $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+//   return false;
+// });​
